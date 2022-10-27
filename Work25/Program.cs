96554414e0dc44ffcae3f4c7858c3025cd6eee22
DenisIG333 <ignatovdnc@inbox.ru>
2022-10-27 10:51:49 +0300

    домашнее задание (семинар за 23.10.22)

---
+++ Work25/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+int raiseToPow()
+{
+    Console.WriteLine("Введите число А");
+    int numberA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число B");
+    int numberB = int.Parse(Console.ReadLine());
+
+    int result = 1;
+
+    for (int i=1; i <= numberB; i++)
+    {
+        result = result*numberA;
+    }
+    return(result);
+}
+
+int res = raiseToPow();
+Console.WriteLine($"число A в натуральной степени B = {res}");
---
+++ Work27/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+int SummDigit()
+{
+    Console.WriteLine("Введите число");
+    int digit = int.Parse(Console.ReadLine());
+
+    int result = 0;
+    for (int i = 0; digit > 0; i++)
+    {
+        result = result + digit % 10;
+        digit = digit / 10;
+    }
+
+    return result;
+}
+
+int res = SummDigit();
+Console.WriteLine($"сумма цифр = {res}");
